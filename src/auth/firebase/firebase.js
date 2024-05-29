e07a36c8356aeb8beb9a246df48e4be37ad766f2
Carolina <carovel2608@gmail.com>
2024-05-29 15:45:49 -0500

    feat: firebase install

---
+++ src/auth/firebase/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAMsALwxDRslktMTkNE35rY3C01wVBKmg",
+  authDomain: "the-people-4587f.firebaseapp.com",
+  projectId: "the-people-4587f",
+  storageBucket: "the-people-4587f.appspot.com",
+  messagingSenderId: "626889009551",
+  appId: "1:626889009551:web:76616e6d5722826b8d03fa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
