c631f5ad8caf07be42deef91a3701df2aa6a2d82
Lina <limapa1220@gmail.com>
2024-05-30 17:15:04 -0500

    fix: project card

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import "./ProjectCard.css";
 
-import { PROJECT_DATA } from "../../Cons/PROJECT_DATA";
-import { useNavigate } from "react-router-dom";
-
 export function ProjectCard(props) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/show")
-}
-
-  const { id, p, src, alt } = props;
+  const { p, src, alt } = props;
   return (
-    <div className="project-img-container">
-      {PROJECT_DATA.map(({ id, p, src, alt }) => (
-        <div className="project-card" key={id}>
-          <div className="tag-div">
-            <p>{p}</p>
-          </div>
-          <img className="project-img" onClick={handleClick} 
-          src={src} alt={alt} />
-        </div>
-      ))}
+    <div className="project-card">
+      <div className="tag-div">
+        <p>{p}</p>
+      </div>
+      <img className="project-img" src={src} alt={alt} />
     </div>
   );
 }
